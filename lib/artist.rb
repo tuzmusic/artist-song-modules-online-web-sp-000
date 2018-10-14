13ddd8f5298a201dcc937b010fd6a165f9e867b4
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-14 16:58:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable
+  extend Findable
   attr_accessor :name
   attr_reader :songs
 
@@ -12,14 +13,14 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
   end
-  
+
   def add_song(song)
     @songs << song
     song.artist = self
