fb52a41a6df45acda40870c33d2494b36739b180
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-14 16:55:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,15 +19,7 @@ class Artist
   def self.all
     @@artists
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
-
+  
   def add_song(song)
     @songs << song
     song.artist = self
